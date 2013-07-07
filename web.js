88ac26eb93b3e60b64db5379246b0b2029d1eec3
Ubuntu <ubuntu@ip-172-31-30-81.us-west-1.compute.internal>
2013-07-07 23:18:39 +0000

    Fixed stupid master

--- web.js
+++ web.js
@@ -2,8 +2,8 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
-var str = fs.readFileSync(./index.html);
 app.get('/', function(request, response) {
+  var str = fs.readFileSync('./index.html');
   response.send(str.toString());
 });
 
